b493750ec142e4100903220599d4ff1e504f8954
hongthai <hongthai@vnpt.vn>
2025-09-29 15:30:28 +0700

    feat: implement SSL Certificate Checker UI and enhance host validation

--- index.js
+++ index.js
@@ -5,12 +5,24 @@ const constants = require("constants");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const hostRegex =
+  /^(?!:\/\/)([a-zA-Z0-9-_]+\.)*[a-zA-Z0-9][a-zA-Z0-9-_]+\.[a-zA-Z]{2,11}?$/;
+
 // Endpoint: /check?host=example.com
 app.get("/check", async (req, res) => {
-  const host = req.query.host;
+  let host = req.query.host;
   if (!host) {
     return res.status(400).json({ error: "Missing host param" });
   }
+
+  // validate host
+  if (!hostRegex.test(host)) {
+    return res.status(400).json({ error: "Invalid host" });
+  }
+  // remove http/https
+  host = host.replace(/https?:\/\//, "");
+  // remove trailing slash
+  host = host.replace(/\/.*/, "");
 
   try {
     const socket = tls.connect(
@@ -21,7 +33,7 @@ app.get("/check", async (req, res) => {
         secureOptions:
           constants.SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION |
           constants.SSL_OP_LEGACY_SERVER_CONNECT,
-        minDHSize: 0,
+        minDHSize: 1,
       },
       () => {
         const cert = socket.getPeerCertificate();
@@ -31,7 +43,6 @@ app.get("/check", async (req, res) => {
           return res.status(500).json({ error: "Cannot get certificate info" });
         }
 
-        // notAfter = ngày hết hạn chứng chỉ (string)
         const expiryDate = new Date(cert.valid_to);
 
         res.json({
@@ -56,9 +67,7 @@ app.get("/check", async (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  res.send(
-    "SSL Check API. Use /check?host=example.com to check SSL certificate."
-  );
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(PORT, () => {
